25e78e51c05c2ed5882a62c4c368a0b8c498d7ff
Ezequiel Giannoni <egiannoni@frba.utn.edu.ar>
2023-02-14 22:07:15 -0300

    Ruta a Contacto

--- SyG/src/app/app-routing.module.ts
+++ SyG/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './contacto/contacto.component';
+import { ProyectoComponent } from './proyecto/proyecto.component';
 
-const routes: Routes = [];
+const routes: Routes = 
+[
+  // HOME
+  {path: '', component: ProyectoComponent },
+  // CONTACTO
+  {path: 'contact', component: ContactoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- SyG/src/app/banner/banner.component.ts
+++ SyG/src/app/banner/banner.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-banner',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent {
+
+  constructor(
+    private router: Router,
+  ) {}
+
+  Contacto(){
+    this.router.navigate(["contact"])
+  }
 
 }
